df568cb89a1b6bdc8839a7cce05cec6582d1af02
Rafi1702 <rafirizqi56@gmail.com>
2026-01-28 15:37:45 +0700

    delete unused object

--- app/src/main/java/com/example/pengingatku/utils/State.kt
+++ app/src/main/java/com/example/pengingatku/utils/State.kt
@@ -4,5 +4,4 @@ sealed class StateHelper<out T>{
     object Loading: StateHelper<Nothing>()
     class Success<T>(val data:T ) : StateHelper<T>()
     class Failure(val exception: Exception): StateHelper<Nothing>()
-    companion object
 }
